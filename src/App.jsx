6c9c513d8411cd4cfa44b38d95d4bd1de0b5094d
j-jinjia <j.jinjia99@gmail.com>
2022-03-21 16:46:13 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
+import Services from "./component/Services/Services";
+import NotFound from "./component/NotFound/NotFound";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
