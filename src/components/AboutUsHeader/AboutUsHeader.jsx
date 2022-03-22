4395060401660401552b285ce137ed5af71a0ebb
Paul <paulmichaelhardman@gmail.com>
2022-03-22 15:12:10 +0000

    Added flex and elements

--- src/components/AboutUsHeader/AboutUsHeader.jsx
+++ src/components/AboutUsHeader/AboutUsHeader.jsx
@@ -1,21 +1,30 @@
 import Hero from "../Hero/Hero";
 import "./AboutUsHeader.scss";
+import AboutMe from "../../assets/images/SitePictures/AboutMe.png";
 
 const AboutUsHeader = () => {
   return (
     <div className="about-us-header">
-      <Hero
-        header={
-          <>
-            <h2 className="hero__header">Hey there</h2>
-            <h2 className="hero__header">My name is Melanie!</h2>
-          </>
-        }
-        subheaderText={
-          "As the owner of The Positive Pup, it is my joy to be able to welcome you to this website."
-        }
-        buttonText={"More About Us"}
-        isSecondary={true}
+      <main className="about-us-header__content">
+        <h1 className="about-us-header__content-title">ABOUT US</h1>
+        <Hero
+          header={
+            <>
+              <h2 className="hero__header">Hey there</h2>
+              <h2 className="hero__header">My name is Melanie!</h2>
+            </>
+          }
+          subheaderText={
+            "As the owner of The Positive Pup, it is my joy to be able to welcome you to this website."
+          }
+          buttonText={"More About Us"}
+          isSecondary={true}
+        />
+      </main>
+      <img
+        src={AboutMe}
+        alt="Melanie and a dog"
+        className="about-us-header__image"
       />
     </div>
   );
