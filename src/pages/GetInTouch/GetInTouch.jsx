a84a059b5a7de06e7cb660bafcdc6b0a7cbce679
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-03-23 15:11:34 +0000

    created, organised and styled component

--- src/pages/GetInTouch/GetInTouch.jsx
+++ src/pages/GetInTouch/GetInTouch.jsx
@@ -1,5 +1,12 @@
+import ContactHeader from "../../components/ContactHeader/ContactHeader";
+
 const GetInTouch = () => {
-  return <h1>Get in touch</h1>;
+  return (
+    <>
+      <h1>Get in touch</h1>
+      <ContactHeader />
+    </>
+  );
 };
 
 export default GetInTouch;
