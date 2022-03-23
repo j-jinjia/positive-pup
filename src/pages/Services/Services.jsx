19c43f2d5d525513078fa27edab2471f66aa9bff
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-23 14:22:26 +0000

    Started on services header

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,5 +1,12 @@
+import "./Services.scss";
+
 const Services = () => {
-  return <h1>Services</h1>;
+  return (
+    <div className="services-header">
+      <h1 className="services-header__heading">Our Services</h1>
+      <h2 className="services-header__subheading">Take a peek at everything we offer</h2>
+    </div>
+  )
 };
 
 export default Services;
