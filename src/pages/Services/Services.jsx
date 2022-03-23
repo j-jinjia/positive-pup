d122930bf953d1e2ab70c0ee6dedd26cc2166735
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-23 15:15:07 +0000

    First draft of services header

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -3,7 +3,7 @@ import "./Services.scss";
 const Services = () => {
   return (
     <div className="services-header">
-      <h1 className="services-header__heading">Our Services</h1>
+      <h1 className="services-header__heading">OUR SERVICES</h1>
       <h2 className="services-header__subheading">Take a peek at everything we offer</h2>
     </div>
   )
