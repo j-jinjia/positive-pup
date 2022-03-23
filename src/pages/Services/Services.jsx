ffb03a09ec24edc1b30f678756134a684619607d
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-23 15:19:46 +0000

    Changed class names

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,9 +2,9 @@ import "./Services.scss";
 
 const Services = () => {
   return (
-    <div className="services-header">
-      <h1 className="services-header__heading">OUR SERVICES</h1>
-      <h2 className="services-header__subheading">Take a peek at everything we offer</h2>
+    <div className="services">
+      <h1 className="services__heading">OUR SERVICES</h1>
+      <h2 className="services__subheading">Take a peek at everything we offer</h2>
     </div>
   )
 };
