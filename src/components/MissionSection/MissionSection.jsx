17481650b83012d75a7e2ac7158e2acbf7d3aad8
Ahmed Omar <ahmedomar@hey.com>
2022-03-24 14:23:25 +0000

    Created MissionSection Card

---
+++ src/components/MissionSection/MissionSection.jsx
@@ -0,0 +1,18 @@
+import "./MissionSection.scss";
+
+const MissionSection = () => {
+  return (
+    <div className="MissionSection">
+    <h1 className="MissionSection__heading"> </h1>
+
+    <p className="MissionSection__about-us"> </p>
+    
+    <a>
+      
+    </a>
+    <div/>
+    
+  )
+};
+
+export default MissionSection;
