0b1f6b01ee158f62eadc724955e74b0dc59327a8
j-jinjia <j.jinjia99@gmail.com>
2022-03-28 15:52:35 +0100

    my mission section + stylings and typography title

--- src/components/MissionSection/MissionSection.jsx
+++ src/components/MissionSection/MissionSection.jsx
@@ -3,13 +3,14 @@ import arrow from "../../assets/svgs/arrow.svg";
 
 const MissionSection = () => {
   return (
-    <div className="MissionSection">
-      <h1 className="MissionSection__heading"> My Mission</h1>
-      <p className="MissionSection__about-us">
+    <div className="mission-section">
+      <h1 className="mission-section__heading"> My Mission</h1>
+      <p className="mission-section__about-us">
         Have you ever dreamt about your perfect career? the kind of job that
         would make you jump out of bed in the morning, ready to welcome in the
         new day? Mine has always involved working with dogs and making a
         difference to people’s lives. <br />
+        <br />
         I have made it my mission to help promote and strengthen the bond
         between humans and their canine companions, so that I can make a
         difference in your world. I truly believe that by increasing the
@@ -17,11 +18,17 @@ const MissionSection = () => {
         can be achieved. The results speak for themselves, content well-mannered
         canines and relaxed happy humans!
         <br />
+        <br />
         Thanks for visiting The Positive Pup and for taking your very first step
         towards building a special bond with your canine companion.
-        <br />
       </p>
-      <a> View Training Methods </a> <img src={arrow} />
+      <div className="link-container">
+        <a className="link-container__link" href="">
+          {" "}
+          View Training Methods{" "}
+        </a>{" "}
+        <img className="link-container__arrow" src={arrow} />
+      </div>
     </div>
   );
 };
