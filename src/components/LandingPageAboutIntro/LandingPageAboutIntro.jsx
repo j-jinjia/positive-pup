6fedf1839ce9ba3afdb3c47a76b6318063096d44
CameronHyden <camh.hyden@gmail.comcamh.hyden@gmail.comclear>
2022-03-29 15:08:46 +0100

    link fix

--- src/components/LandingPageAboutIntro/LandingPageAboutIntro.jsx
+++ src/components/LandingPageAboutIntro/LandingPageAboutIntro.jsx
@@ -2,36 +2,33 @@ import "./LandingPageAboutIntro.scss";
 import introImage from "../../assets/images/landing_page/landing_page_about_intro_img.png";
 import Button from "../Button/Button";
 import paw from "../../assets/images/landing_page/paw_print.svg";
-import { Link } from "react-router-dom";
 
 const LandingPageAboutIntro = () => {
   return (
-      <div className="landing-page-about-intro">
-          <img
-            src={paw}
-            className="landing-page-about-intro__paw"
-            alt="paw print"
-          />
-          <h2 className="landing-page-about-intro__header">
-            Why The Positive Pup?
-          </h2>
-          <p className="landing-page-about-intro__paragraph">
-            {" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi
-            malesuada tortor, tristique aenean ultricies. Integer aliquam mattis
-            turpis justo, faucibus bibendum sagittis lacus ornare. Porttitor
-            eget sed amet malesuada est dignissim magna. Amet vel tempor egestas
-            at in mi, facilisi proin.
-          </p>
-          <Link to="/about" className="landing-page-about-intro__button">
-            <Button buttonClassName="landing-page-about-intro__button" isSecondary={true} buttonText="More About Me" />
-          </Link>
-          <img
-              src={introImage}
-              className="landing-page-about-intro__image"
-              alt="Positive Pup owner, Melanie, with a dog"
-          />
-      </div>
+    <div className="landing-page-about-intro">
+      <img
+        src={paw}
+        className="landing-page-about-intro__paw"
+        alt="paw print"
+      />
+      <h2 className="landing-page-about-intro__header">
+        Why The Positive Pup?
+      </h2>
+      <p className="landing-page-about-intro__paragraph">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi malesuada
+        tortor, tristique aenean ultricies. Integer aliquam mattis turpis justo,
+        faucibus bibendum sagittis lacus ornare. Porttitor eget sed amet
+        malesuada est dignissim magna. Amet vel tempor egestas at in mi,
+        facilisi proin.
+      </p>
+      <Button link="/about" isSecondary={true} buttonText="More About Me" />
+      <img
+        src={introImage}
+        className="landing-page-about-intro__image"
+        alt="Positive Pup owner, Melanie, with a dog"
+      />
+    </div>
   );
 };
 
