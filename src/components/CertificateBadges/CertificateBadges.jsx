1ec6f48409eb591a1aa0be09b29b9b89dd1497cc
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-30 15:50:27 +0100

    Refactored code to hide text without using display: none

--- src/components/CertificateBadges/CertificateBadges.jsx
+++ src/components/CertificateBadges/CertificateBadges.jsx
@@ -4,22 +4,26 @@ import ppgBadge from "../../assets/images/certification_badges/PPG_Member_Badge.
 import ukBehaviour from "../../assets/images/certification_badges/UK_dog_behaviour.png";
 import victoriaStillwell from "../../assets/images/certification_badges/victoria_stillwell.png";
 
-
-const CertificateBadges = (props) => {
-
-  const {showText} = props;
-  let certificateClassName = "certification-section";
-  certificateClassName += showText ? " certification-section--show" : " certification-section--hide";
-
-
+const CertificateBadges = ({ showText }) => {
   return (
-    <>
-      <div className={certificateClassName}>
-        <h2 className="certification-section__header">You’re in professional hands!</h2>
-        <p className="certification-section__paragraph">I’m a well-established, certified trainer in Dog behaviour &#38; training, withover  <span className="certification-section__paragraph--highlighted-text">4.5 years of experience.</span> To find out moreinformation  about any of my certifications, please click on one of the logos.</p>
-      </div>    
-
-      <div className="certificate-badges">
+    <div className="certifications">
+      {showText && (
+        <>
+          <h2 className="certifications__header">
+            You’re in professional hands!
+          </h2>
+          <p className="certifications__description">
+            I’m a well-established, certified trainer in Dog behaviour &#38;
+            training, withover{" "}
+            <span className="certifications__description--highlighted-text">
+              4.5 years of experience.
+            </span>{" "}
+            To find out more information about any of my certifications, please
+            click on one of the logos.
+          </p>
+        </>
+      )}
+      <div className="certifications__badge-container">
         <a
           href="https://www.vsdogtrainingacademy.com/"
           rel="noreferrer"
@@ -27,7 +31,7 @@ const CertificateBadges = (props) => {
         >
           <img
             src={victoriaStillwell}
-            className="certificate-badges__img"
+            className="certifications__badge"
             alt="Victoria Stillwell certification badge"
           />
         </a>
@@ -38,14 +42,14 @@ const CertificateBadges = (props) => {
         >
           <img
             src={internationalBadge}
-            className="certificate-badges__img"
+            className="certifications__badge"
             alt="international companion animal network certification badge"
           />
         </a>
         <a href="https://ukdogcharter.org/" rel="noreferrer" target="_blank">
           <img
             src={ukBehaviour}
-            className="certificate-badges__img"
+            className="certifications__badge"
             alt="UK dog behaviour and training charter certification badge"
           />
         </a>
@@ -56,12 +60,12 @@ const CertificateBadges = (props) => {
         >
           <img
             src={ppgBadge}
-            className="certificate-badges__img"
+            className="certifications__badge"
             alt="the pet professional guild certification badge"
           />
         </a>
       </div>
-    </>
+    </div>
   );
 };
 
