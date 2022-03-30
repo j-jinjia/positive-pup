fbad60a2149dccb4619120c58f050217c3e6e101
Shreeya <shreeya@Shreeyas-MacBook-Pro.local>
2022-03-30 17:18:49 +0100

    added styling at mobile and tablet

--- src/components/AboutUsTrainingSection/AboutUsTrainingSection.jsx
+++ src/components/AboutUsTrainingSection/AboutUsTrainingSection.jsx
@@ -3,22 +3,25 @@ import "./AboutUsTrainingSection.scss";
 
 const AboutUsTrainingSection = () => {
   return (
-    <div className="landing-page-about-intro">
-      <h2 className="landing-page-about-intro__header">Training Methods </h2>
+    <div className="about-us-training-section">
       <img
         src={trainingMethods}
-        className="landing-page-about-intro__image"
+        className="about-us-training-section__image"
         alt="Positive Pup owner, Melanie, with a dog"
       />
 
-      <p className="landing-page-about-intro__paragraph">
+      <h2 className="about-us-training-section__header">Training Methods </h2>
+      <p className="about-us-training-section__paragraph">
         As a graduate of the Victoria Stilwell Dog Training Academy, I am a
         positive reinforcement dog trainer. This means that I use reward-based
         training methods that are deeply rooted in the continually evolving
-        world of behavioral science. Outdated old school dominance and
-        punishment are certainly not my bag. Not only does science tell us that
-        these methods achieve temporary results and can often result in eventual
-        aggressive behavior, but that they are cruel, dangerous and unnecessary.
+        world of behavioral science.
+      </p>
+      <p className="about-us-training-section__paragraph">
+        Outdated old school dominance and punishment are certainly not my bag.
+        Not only does science tell us that these methods achieve temporary
+        results and can often result in eventual aggressive behavior, but that
+        they are cruel, dangerous and unnecessary.
       </p>
     </div>
   );
