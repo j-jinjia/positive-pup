6a770d5dc847cb94e8aa46449a012f905a145771
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-30 17:03:15 +0100

    Started responding to comments

--- src/components/ServicesCard/ServicesCard.jsx
+++ src/components/ServicesCard/ServicesCard.jsx
@@ -1,27 +1,18 @@
 import "./ServicesCard.scss";
 import vectorBlack from "../../assets/svgs/Vector-black.svg";
-import vectorWhite from "../../assets/svgs/Vector-white.svg";
 import { Link } from "react-router-dom";
 
 const ServicesCard = ({ title, description }) => {
   return (
     <div className="services-card">
-      <h2 className="services-card__title">{title}</h2>
-      <h3>{description}</h3>
-      <Link to="/services">
-        <div className="services-card__button">
-          <img
-            className="services-card__vector services-card__vector--black"
-            src={vectorBlack}
-            alt=""
-          />
-          {/* Put a second chevron here of a different colour with display=none. We can use this to change the colour when we add hover later */}
-          <img
-            className="services-card__vector services-card__vector--white"
-            src={vectorWhite}
-            alt=""
-          />
-        </div>
+      <h3 className="services-card__title">{title}</h3>
+      <p>{description}</p>
+      <Link to="/services" className="services-card__button">
+        <img
+          className="services-card__vector services-card__vector--black"
+          src={vectorBlack}
+          alt=""
+        />
       </Link>
     </div>
   );
--- src/components/ServicesSection/ServicesSection.jsx
+++ src/components/ServicesSection/ServicesSection.jsx
@@ -6,9 +6,9 @@ const ServicesSection = ({ link }) => {
   return (
     <div className="services-section">
       <h2 className="services-section__header">Services I Offer</h2>
-      <h3 className="services-section__sub-header">
+      <p className="services-section__sub-header">
         A breakdown of all the different kind of services I offer.
-      </h3>
+      </p>
       <hr className="services-section__line-break"></hr>
       <div className="services-section__course-container">
         <ServicesCard
@@ -27,12 +27,7 @@ const ServicesSection = ({ link }) => {
           ipsum saepe ipsa optio dolorem veniam tenetur nemo labore ut"
         />
       </div>
-      <Button
-        className="services-section__button"
-        buttonText="VIEW OUR SERVICES"
-        isSecondary={true}
-        link={link}
-      />
+      <Button buttonText="VIEW OUR SERVICES" isSecondary={true} link={link} />
     </div>
   );
 };
