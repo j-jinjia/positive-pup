66b194dd456acc2453ef830685459153e2547375
kirstytheproud <k.joproud@gmail.com>
2022-04-04 15:33:27 +0100

    added search bar

---
+++ src/components/ServicesSearchBar/ServicesSearchBar.jsx
@@ -0,0 +1,13 @@
+import "./ServicesSearchBar.scss"
+import searchIcon from "../../assets/svgs/searchBarIcon.svg"
+
+const ServicesSearchBar = ({searchTerm, handleInput}) => {
+  return (
+    <div className="search-box">
+        <input className="search-box__input" type="text" value={searchTerm}  onChange={handleInput}/>
+        <img className="search-box__input--icon" src={searchIcon} alt="search icon" />
+        </div>
+  )
+}
+
+export default ServicesSearchBar;
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,8 +2,27 @@ import Layout from "../../components/Layout/Layout";
 import "./Services.scss";
 import { CoursesList } from "../../Containers/CoursesList/CoursesList";
 import courseData from "../../assets/mockData/courseData";
+import ServicesSearchBar from "../../components/ServicesSearchBar/ServicesSearchBar";
+import { useState } from "react";
 
 const Services = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInput = (event) => {
+    const cleanInput = event.target.value.toLowerCase();
+    setSearchTerm(cleanInput);
+  };
+
+  
+  const filteredSearchItems = courseData.filter((course) => {
+    const filteredCourseHeading = course.courseHeading.toLowerCase();
+    // const filteredCourseLabel = course.labelsArray.toLowerCase();
+    return filteredCourseHeading.includes(searchTerm) 
+    // || filteredCourseLabel.includes(searchTerm);
+  });
+
+
+
   return (
     <Layout>
       <div className="services">
@@ -11,7 +30,8 @@ const Services = () => {
         <h2 className="services__subheading">
           Take a peek at everything we offer
         </h2>
-        <CoursesList courseData={courseData} />
+        <ServicesSearchBar searchTerm={searchTerm} handleInput={handleInput}/>
+        <CoursesList courseData={filteredSearchItems} />
       </div>
     </Layout>
   );
