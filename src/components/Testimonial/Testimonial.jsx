cdb999b17907d0a3a1857ee3b49da84c3b2dcb98
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-04-06 14:46:52 +0100

    resolved bug by adding show more feature

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -2,9 +2,19 @@ import "./Testimonial.scss";
 import calendar from "../../assets/svgs/calendar.svg";
 import folder from "../../assets/svgs/testimonial_course.svg";
 import quotations from "../../assets/svgs/quotations-1.svg";
+import { useState } from "react";
 
 const Testimonial = ({ name, date, course, comment, active }) => {
   const cardClass = active ? "testimonial testimonial--active" : "testimonial";
+  const [filterComment, setFilterComment] = useState(true);
+
+  const charLimit = 400;
+  const filteredComment = () => {
+    return comment.slice(0, charLimit) + "...";
+  };
+  const handleClick = () => {
+    setFilterComment(!filterComment);
+  };
 
   return (
     <div className={cardClass} data-testid={name}>
@@ -17,7 +27,16 @@ const Testimonial = ({ name, date, course, comment, active }) => {
           <img src={folder} alt="" /> {course}
         </p>
       </div>
-      <p className="testimonial__comment">{comment}</p>
+      <p className="testimonial__comment">
+        {comment.length > charLimit && filterComment
+          ? filteredComment()
+          : comment}
+        {comment.length > charLimit && (
+          <button className="testimonial__comment-button" onClick={handleClick}>
+            {filterComment ? "show more" : "show less"}
+          </button>
+        )}
+      </p>
       <img
         className="testimonial__quotations testimonial__quotations--1"
         src={quotations}
