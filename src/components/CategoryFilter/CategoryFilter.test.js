5c654e53fd0b7f732a02de9f138a255b11095d88
Paul <paulmichaelhardman@gmail.com>
2022-04-06 16:55:17 +0100

    nice

--- src/components/CategoryFilter/CategoryFilter.test.js
+++ src/components/CategoryFilter/CategoryFilter.test.js
@@ -2,13 +2,17 @@ import { customRender } from "../../utils/testUtils";
 import CategoryFilter from "./CategoryFilter";
 
 it("should match CategoryFilter component snapshot", () => {
-  const { container } = customRender(<CategoryFilter courseType={"All"}
-    filterOptions={[
-      "All",
-      "Online Courses",
-      "Group Classes",
-      "Bespoke Packages",
-    ]} />);
+  const { container } = customRender(
+    <CategoryFilter
+      courseType={"All"}
+      filterOptions={[
+        "All",
+        "Online Courses",
+        "Group Classes",
+        "Bespoke Packages",
+      ]}
+    />
+  );
 
   expect(container).toMatchSnapshot();
 });
