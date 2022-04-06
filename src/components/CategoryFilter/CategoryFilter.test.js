fd0c7fb3db29cb5961e70bf9edcc5f54e429e8ef
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-04-06 15:37:38 +0100

    Updated tests

--- src/components/CategoryFilter/CategoryFilter.test.js
+++ src/components/CategoryFilter/CategoryFilter.test.js
@@ -2,7 +2,13 @@ import { customRender } from "../../utils/testUtils";
 import CategoryFilter from "./CategoryFilter";
 
 it("should match CategoryFilter component snapshot", () => {
-  const { container } = customRender(<CategoryFilter />);
+  const { container } = customRender(<CategoryFilter courseType={"All"}
+    filterOptions={[
+      "All",
+      "Online Courses",
+      "Group Classes",
+      "Bespoke Packages",
+    ]} />);
 
   expect(container).toMatchSnapshot();
 });
