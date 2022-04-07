a71d2c9e22b8675ec1ed3d59d7a21e7c3b3bbcf2
Ahmed Omar <ahmedomar@hey.com>
2022-04-07 15:42:36 +0100

    Deleted Services.scss and corrosponding import into Services.jsx

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,5 +1,4 @@
 import Layout from "../../components/Layout/Layout";
-import "./Services.scss";
 import Header from "../../components/Header/Header";
 import { CoursesList } from "../../Containers/CoursesList/CoursesList";
 import courseData from "../../assets/mockData/courseData";
