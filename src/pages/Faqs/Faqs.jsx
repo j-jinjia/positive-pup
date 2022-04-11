2a71aa609a67949237a35941f4055b2ebb0d1a9d
j-jinjia <j.jinjia99@gmail.com>
2022-04-11 17:17:28 +0200

    initial commit on filters

--- src/pages/Faqs/Faqs.jsx
+++ src/pages/Faqs/Faqs.jsx
@@ -1,9 +1,27 @@
+import CategoryFilter from "../../components/CategoryFilter/CategoryFilter";
+// import { useEffect, useState } from "react/cjs/react.production.min";
 import Layout from "../../components/Layout/Layout";
 
 const Faqs = () => {
+  // const [faqType, setFaqType] = useState("General");
+  const filterOptions = [
+    "General",
+    "Payments ",
+    "Training Methods",
+    "Packages ",
+    "Group Classes",
+  ];
+
+  const handleClick = () => {};
+
   return (
     <Layout>
       <h1>FAQs</h1>
+      <CategoryFilter
+        courseType={""}
+        handleClick={handleClick}
+        filterOptions={filterOptions}
+      />
     </Layout>
   );
 };
