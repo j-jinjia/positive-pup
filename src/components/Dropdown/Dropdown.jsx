0f34753591063e66f21b0c6c709a7b7596f94882
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-04-11 15:57:39 +0100

    created components and wrote tests

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,42 @@
+import "./Dropdown.scss";
+import { useState } from "react";
+import React from "react";
+
+const Dropdown = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const mainClass = isOpen
+    ? "dropdown__main dropdown__main--active"
+    : "dropdown__main";
+
+  const answerArr = answer.split("\n");
+  const answerText = answerArr.map((l, i) => (
+    <React.Fragment key={l}>
+      <p className="dropdown__main-text">{l}</p>
+      {i !== answerArr.length - 1 && <br />}
+    </React.Fragment>
+  ));
+
+  return (
+    <div className="dropdown">
+      <label className="dropdown__heading-label" htmlFor="dropdown">
+        {"Click to view answer to " + question}
+      </label>
+      <button
+        id="dropdown"
+        className="dropdown__heading"
+        onClick={toggleDropdown}
+      >
+        <h3 className="dropdown__heading-title">{question}</h3>
+        <p className="dropdown__heading-action">{isOpen ? "-" : "+"}</p>
+      </button>
+      <div className={mainClass}>{answerText}</div>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/pages/Faqs/Faqs.jsx
+++ src/pages/Faqs/Faqs.jsx
@@ -1,9 +1,16 @@
+import Dropdown from "../../components/Dropdown/Dropdown";
 import Layout from "../../components/Layout/Layout";
+import faqsData from "../../assets/mockData/faqsData";
 
 const Faqs = () => {
+  const faqSection = faqsData.map((d) => (
+    <Dropdown key={d.id} question={d.question} answer={d.answer} />
+  ));
+
   return (
     <Layout>
       <h1>FAQs</h1>
+      {faqSection}
     </Layout>
   );
 };
