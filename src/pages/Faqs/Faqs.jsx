9a1deafb62b1b459e7cee7fce01388d967ac66c7
j-jinjia <j.jinjia99@gmail.com>
2022-04-12 15:58:48 +0200

    finished filters faq

--- src/pages/Faqs/Faqs.jsx
+++ src/pages/Faqs/Faqs.jsx
@@ -1,24 +1,23 @@
 import CategoryFilter from "../../components/CategoryFilter/CategoryFilter";
-import { useState } from "react/cjs/react.production.min";
+import { useState } from "react";
 import Dropdown from "../../components/Dropdown/Dropdown";
 import Header from "../../components/Header/Header";
 import Layout from "../../components/Layout/Layout";
 import faqsData from "../../assets/mockData/faqsData";
+import "./Faq.scss";
 
 const Faqs = () => {
-  const [faqType, setFaqType] = useState("General");
+  const [faqType, setFaqType] = useState("General /");
   const filterOptions = [
-    "General",
-    "Payments",
-    "Training Methods",
-    "Packages",
+    "General /",
+    "Payments /",
+    "Training Methods /",
+    "Packages /",
     "Group Classes",
   ];
 
-  // no useffect, re render on usestate change
-
   const filteredQuestions = faqsData.filter((question) => {
-    if (faqType === "General") return true;
+    if (faqType === "General /") return true;
     return question.type === faqType;
   });
 
@@ -28,7 +27,6 @@ const Faqs = () => {
     }
   };
 
-  //pass in filtered data.
   const faqSection = filteredQuestions.map((data) => (
     <Dropdown key={data.id} question={data.question} answer={data.answer} />
   ));
@@ -36,11 +34,6 @@ const Faqs = () => {
   return (
     <Layout>
       <h1>FAQs</h1>
-      <CategoryFilter
-        courseType={faqType}
-        handleClick={handleClick}
-        filterOptions={filterOptions}
-      />
 
       <Header
         isLeftAlign={false}
@@ -48,7 +41,13 @@ const Faqs = () => {
         subheadingText={"Some of your most common questions"}
         isAlternative={true}
       />
-      {faqSection}
+      <CategoryFilter
+        courseType={faqType}
+        handleClick={handleClick}
+        filterOptions={filterOptions}
+        isAlternativeHighlight
+      />
+      <div className="faq-section">{faqSection}</div>
     </Layout>
   );
 };
