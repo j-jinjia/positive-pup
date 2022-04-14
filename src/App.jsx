9617b26b3293498888ecc18b2a49698242bd88c2
Ahmed Omar <ahmedomar@hey.com>
2022-04-14 14:45:51 +0100

    deleted gallery and testimonials pages. Removed all references and routing for these pages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import "./styles/main.scss";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Services from "./pages/Services/Services";
-import Testimonials from "./pages/Testimonials/Testimonials";
-import Gallery from "./pages/Gallery/Gallery";
 import GetInTouch from "./pages/GetInTouch/GetInTouch";
 import BookNow from "./pages/BookNow/BookNow";
 import NotFound from "./pages/NotFound/NotFound";
@@ -18,8 +16,6 @@ const App = () => {
       <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
       <Route path="/services/:filter" element={<Services />} />
-      <Route path="/testimonials" element={<Testimonials />} />
-      <Route path="/gallery" element={<Gallery />} />
       <Route path="/get-in-touch" element={<GetInTouch />} />
       <Route path="/book-now" element={<BookNow />} />
       <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
