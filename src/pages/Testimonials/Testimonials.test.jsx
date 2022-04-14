c4a8576022866c3c6a14f0e0a1762aa312abb2a5
Ahmed Omar <ahmedomar@hey.com>
2022-04-14 14:46:32 +0100

    Deleted Gallery and Testimonials

--- src/pages/Testimonials/Testimonials.test.jsx
+++
@@ -1,8 +0,0 @@
-import { customRender } from "../../utils/testUtils";
-import Testimonials from "./Testimonials";
-
-it("should render the testimonials page", () => {
-  const { container } = customRender(<Testimonials />);
-
-  expect(container).toMatchSnapshot();
-});
